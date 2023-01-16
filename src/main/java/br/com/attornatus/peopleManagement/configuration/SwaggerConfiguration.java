fc5010c3ce4cd300811fe4dbbff30bdc6af1bb5e
Victor Henrique <vhsdm7@gmail.com>
2023-01-16 11:05:07 -0300

    Finalizada doc da API

--- src/main/java/br/com/attornatus/peopleManagement/configuration/SwaggerConfiguration.java
+++ src/main/java/br/com/attornatus/peopleManagement/configuration/SwaggerConfiguration.java
@@ -2,19 +2,33 @@ package br.com.attornatus.peopleManagement.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.*;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfiguration {
 
     @Bean
     public Docket swagger() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(ApiInfo.DEFAULT)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.attornatus.peopleManagement.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("People Management API")
+                .description("Attornatus Backend Developer Assessment")
+                .version("1.0.0")
+                .contact(new Contact("Victor Henrique","https://github.com/viccttor","vhsdm7@gmail.com"))
                 .build();
     }
 }
